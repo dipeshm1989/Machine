385f7cdf22a84cea0dc9acfec7d46a0ca5fed064
DIPESH MATHEMA <dipeshmathema@localhost.localdomain>
2018-01-24 21:57:57 -0500

    Information about  environment

---
+++ env.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+clear
+
+echo "this script will give us the environment information"
+echo "==============================================================="
+echo""
+echo "hello username: $USER"
+echo ""
+echo "Your home directory is: $HOME"
+echo ""
+echo "Your terminal session type is: $Term"
+echo ""
+#en
