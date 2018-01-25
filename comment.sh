5b4511826aaad4f754b1f5542f9cbf750cb326ae
DIPESH MATHEMA <dipeshmathema@localhost.localdomain>
2018-01-24 21:39:39 -0500

    My first shell scripting

---
+++ comment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#This line is intended to be used as a general description of the script and anything that it does
+
+clear           #clears the screen
+
+MYUSERNAME="DIpesH"   #THE USER FOR THIS APPLICATION
+
+echo "We are using the default user called: $MYUSERNAME" #DISPLAY TO THE CONSOLE
+
+DATETIMESTAMP='date'
+
+echo "this script is the time stamp for run : $DATETIMESTAMP"
+
+#END
+
+
